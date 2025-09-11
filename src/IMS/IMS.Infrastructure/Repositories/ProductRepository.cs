8b7c9c6ebf0ebc7bf9ac0bd06bc3b08f27c4ea29
Saroyar-Islam-Sakib <sisakib232@gmail.com>
2025-09-12 01:52:23 +0600

    Initial commit

---
+++ src/IMS/IMS.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using IMS.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Infrastructure.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+    }
+}
---
+++ src/IMS/IMS.Web/Program.cs
@@ -0,0 +1,92 @@
+using IMS.Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using System.Reflection;
+
+#region Configure BootstrapLogger
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.File("Logs/web-log-.log", rollingInterval: RollingInterval.Day)
+    .CreateBootstrapLogger();
+#endregion
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    #region Serilog Configuration
+    builder.Host.UseSerilog((context, lc) => lc
+        .MinimumLevel.Debug()
+        .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Warning)
+        .Enrich.FromLogContext()
+        .ReadFrom.Configuration(builder.Configuration)
+    );
+    #endregion
+
+    // Add services to the container.
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+    
+    //-----Get Migration Assembly-------
+    var migrationAssembly = Assembly.GetAssembly(typeof(ApplicationDbContext));
+
+    //-----Dependency Injection For DbContext-------
+    builder.Services.AddScoped<ApplicationDbContext>(s =>
+        new ApplicationDbContext(connectionString, migrationAssembly?.FullName));
+
+    //--------Add DbContext-----------
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(connectionString, 
+        (x) => x.MigrationsAssembly(migrationAssembly)));
+
+    //-----Add RazorPages()-------
+    builder.Services.AddRazorPages();
+
+
+    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+    builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+        .AddEntityFrameworkStores<ApplicationDbContext>();
+    builder.Services.AddControllersWithViews();
+
+
+    
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseMigrationsEndPoint();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapStaticAssets();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}")
+        .WithStaticAssets();
+
+    app.MapRazorPages()
+       .WithStaticAssets();
+
+    app.Run();
+
+}
+catch(Exception ex)
+{
+    Log.Fatal(ex, "Application crashed");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
